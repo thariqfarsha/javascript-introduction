73b4fe6f8b392efe88f7ba3c8a5af0604748f3de
Thariq Farsha <mthariqfarsha@gmail.com>
2022-03-16 21:20:06 +0700

    Add input validation and multiple error msg handling

--- part-2/task3.js
+++ part-2/task3.js
@@ -1,14 +1,36 @@
 const seleksiNilai = function(nilaiAwal, nilaiAkhir, dataArray){
+    let errorMsg = []
+    
+    // Validasi parameter nilaiAwal harus berupa number
+    if (typeof nilaiAwal !== 'number') {
+        errorMsg.push("Nilai awal harus berupa number")
+    }
+
+    // Validasi parameter nilaiAkhir harus berupa number
+    if (typeof nilaiAkhir !== 'number') {
+        errorMsg.push("Nilai akhir harus berupa number")
+    }
+
+    // Validasi seluruh elemen dataArray harus berupa number
+    if (!dataArray.every(num => typeof num === 'number')) {
+        errorMsg.push("Seluruh value pada data array harus berupa number")
+    }
+
     // Validasi nilaiAwal < nilaiAkhir
     if (nilaiAwal > nilaiAkhir) {
-        return "Nilai akhir harus lebih besar dari nilai awal"
+        errorMsg.push("Nilai akhir harus lebih besar dari nilai awal")
     }
 
     // Validasi data dalam dataArray harus lebih dari 5
     if (dataArray.length <= 5) {
-        return "Jumlah angka dalam dataArray harus lebih dari 5"
+        errorMsg.push("Jumlah angka dalam dataArray harus lebih dari 5")
     }
     
+    if (errorMsg.length > 0) {
+        errorMsg = errorMsg.join("\n")
+        return errorMsg
+    }
+
     let result = []
     dataArray.map((item) => {
         if (item > nilaiAwal && item < nilaiAkhir) {
@@ -24,7 +46,7 @@ const seleksiNilai = function(nilaiAwal, nilaiAkhir, dataArray){
     return result
 }
 
-console.log(seleksiNilai(5, 20 , [2, 25, 4, 14, 17, 30, 8]))
-console.log(seleksiNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8]))
-console.log(seleksiNilai(4, 17 , [2, 25, 4]))
-console.log(seleksiNilai(5, 17 , [2, 25, 4, 1, 30, 18]))
+// console.log(seleksiNilai(5, 20 , [2, 25, 4, 14, 17, 30, 8]))
+// console.log(seleksiNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8]))
+console.log(seleksiNilai(40, "17" , [2, 25, "4"]))
+// console.log(seleksiNilai(5, 17 , [2, 25, 4, 1, 30, 18]))
