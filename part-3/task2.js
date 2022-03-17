a5ec1bbf91c70b457b20bc30c0a800e9a14df1f6
Thariq Farsha <mthariqfarsha@gmail.com>
2022-03-17 20:41:45 +0700

    Finish task 2 js intro part 3

---
+++ part-3/task2.js
@@ -0,0 +1,25 @@
+const getMonth = (callback) => {
+    setTimeout(() => {
+        let error = false
+        let month = [
+            'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli',
+            'Agustus', 'September', 'Oktober', 'November', 'Desember'
+        ]
+
+        if (!error) {
+            callback(null, month)
+        } else {
+            callback(new Error('Sorry data not found'), [])
+        }
+    }, 4000);
+}
+
+function showMonth(error, result) {
+    if (error) {
+        return console.log(error)
+    }
+
+    return result.map(month => console.log(month))
+}
+
+getMonth(showMonth)
