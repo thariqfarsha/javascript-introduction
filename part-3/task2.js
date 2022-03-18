5bbca1915a356fda132b2760017cae660d90247f
Thariq Farsha <mthariqfarsha@gmail.com>
2022-03-18 16:28:57 +0700

    Fix format using prettier

--- part-3/task2.js
+++ part-3/task2.js
@@ -1,25 +1,35 @@
 const getMonth = (callback) => {
-    setTimeout(() => {
-        let error = false
-        let month = [
-            'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli',
-            'Agustus', 'September', 'Oktober', 'November', 'Desember'
-        ]
+  setTimeout(() => {
+    let error = true;
+    let month = [
+      "Januari",
+      "Februari",
+      "Maret",
+      "April",
+      "Mei",
+      "Juni",
+      "Juli",
+      "Agustus",
+      "September",
+      "Oktober",
+      "November",
+      "Desember",
+    ];
 
-        if (!error) {
-            callback(null, month)
-        } else {
-            callback(new Error('Sorry data not found'), [])
-        }
-    }, 4000);
-}
+    if (!error) {
+      callback(null, month);
+    } else {
+      callback(new Error("Sorry data not found"), []);
+    }
+  }, 4000);
+};
 
 function showMonth(error, result) {
-    if (error) {
-        return console.log(error)
-    }
+  if (error) {
+    return console.log(error);
+  }
 
-    return result.map(month => console.log(month))
+  return result.map((month) => console.log(month));
 }
 
-getMonth(showMonth)
+getMonth(showMonth);
